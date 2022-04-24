19210c8bf8b6652cb237965fb9681dfc14da6464
Chr0110 <eradi-@student.1337.ma>
2022-04-24 00:01:01 +0000

    mm

--- sa.c
+++ sa.c
@@ -1,41 +1,30 @@
 #include "push_swap.h"
-
-void ft_creat_lst(t_list **stack_a, t_list *new_node, int t, int ac)
+void ft_checklst(t_list *stack_a)
 {
-	int i;
-	i = 1;
-	new_node = (t_list*)malloc(sizeof(t_list));
-	new_node->content = t;
-	new_node->next = NULL;
-	ft_lstadd_back(stack_a,new_node);
-	
-	while (i < ac)
-	{
-		t_list *new_node2;
-		new_node2 = malloc(sizeof(t_list));
-		new_node2->content = t;
-		new_node2->next = NULL;
-		ft_lstadd_back(stack_a, new_node2);
-		i++;
-		
-	}
-
+	printf("%d\n",(stack_a)->content);
+	printf("%d\n", stack_a->next->content);
+	printf("%d\n", stack_a->next->next->content);
+}
+void ft_creatlst(t_list **stack_a, int t)
+{
+	t_list *new;
+	new = malloc(sizeof(t_list));
+	new->content = t;
+	new->next = NULL;
+	ft_lstadd_back(stack_a, new);
+	return ;
 }
 int main (int ac, char **av)
 {
 	int i;
-	i = 1;
 	t_list *stack_a;
 	stack_a = NULL;
-	t_list *new_node;
-	new_node = (t_list*)malloc(sizeof(t_list));
+	i = 1;
 	while (i < ac)
 	{
-		ft_creat_lst(&stack_a, new_node, atoi(av[i]), ac);
+		ft_creatlst(&stack_a, atoi(av[i]));
 		i++;
 	}
-	printf("%d\n", stack_a->content);
-	printf("%d\n", stack_a->next->content);
-	printf("%d\n", stack_a->next->next->content);
+	ft_checklst(stack_a);
 	return (0);
 }
