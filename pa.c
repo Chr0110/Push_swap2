514d0720c84b5837d0e7f2d04ee04c970a2dcf23
Chr0110 <eradi-@student.1337.ma>
2022-04-27 00:07:39 +0000

    m

---
+++ pa.c
@@ -0,0 +1,15 @@
+ft_pa(**stack_b, **stack_b)
+{
+	t_list *temp;
+	temp = *stack_b;
+	*stack_a = temp;
+}
+int main (int ac, char **av)
+{
+	int i;
+	i = 0;
+	while (i < ac)
+	{
+		
+	}
+}
---
+++ ss.c
@@ -0,0 +1,5 @@
+void ft_ss(**stack_a, **stack_b)
+{
+    ft_sa(&stack_a);
+    ft_sb(&stack_b);
+}
