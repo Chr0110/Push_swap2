ca18072eea111218ee5929d6e06c5c28569cc51a
Chr0110 <eradi-@student.1337.ma>
2022-04-30 08:53:09 +0000

    g

---
+++ rotate_b.c
@@ -0,0 +1,26 @@
+#include "push_swap.h"
+void	ft_rb(t_list **stack_b)
+{
+	t_list *temp;
+	t_list *last;
+	temp = *stack_b;
+	(*stack_b) = (*stack_b)->next;
+	last = ft_lstlast(*stack_b);
+	last->next = temp;
+	temp->next = NULL;
+}
+
+int main (int ac, char **av)
+{
+	t_list *stack_b;
+	int i;
+	i = 1;
+	while (i < ac)
+	{
+		ft_creatlst(&stack_b, atoi(av[i]));
+		i++;
+	}
+	ft_rb(&stack_b);
+	ft_print(stack_b);
+	return (0);
+}
