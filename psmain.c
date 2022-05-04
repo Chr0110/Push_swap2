d0ddc12d7e0ce1ce2cb385b09f0d3b48592030b6
Chr0110 <eradi-@student.1337.ma>
2022-05-04 15:28:24 +0000

    g

--- psmain.c
+++ psmain.c
@@ -2,19 +2,18 @@
 
 int main (int ac, char **av)
 {
-    int i;
-    t_list *stack_a;
-    i = 0;
-    if (ac != 1)
-    {
-        while (i < ac)
-        {
-            ft_creatlst(&stack_a, atoi(av[i]));
-            i++;
-        }
-    }
-    else
-        return (0);
-    ft_print(stack_a);
-    return (0);
+	int i;
+	t_list *stack_a;
+	i = 1;
+	if (ac != 1)
+	{
+		while (i < ac)
+		{
+			ft_creatlst(&stack_a, atoi(av[i]));
+			i++;
+		}
+	}
+	else
+		return (0);
+	return (0);
 }
