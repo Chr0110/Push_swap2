d924b400ce95eee5307256944909265132b3d347
Chr0110 <eradi-@student.1337.ma>
2022-05-10 03:40:58 +0100

    j

--- for100.c
+++ for100.c
@@ -8,19 +8,20 @@ int	howmuch100(t_list *stack_a, int j)
 	int small;
 	len = ft_lstsize(stack_a);
 	small = stack_a->content;
-	while (len > 0)
+	while (len != 0 && stack_a->next != NULL)
 	{
 		if (small > j)
 			stack_a = stack_a->next;
 		else
 		{
-			i++;
 			stack_a = stack_a->next;
+			i++;
 		}
 		small = stack_a->content;
 		len--;
 	}
-	printf("%d\n", i);
+	if (small < j)
+		i++;
 	return (i);
 }
 
@@ -28,11 +29,8 @@ int	index100(t_list *stack_a, int j)
 {
 	int ind;
 	int small;
-	t_list *temp;
 
-	//printf("%d\n", ft_lstsize(stack_a));
 	ind = 1;
-	temp = stack_a;
 	small = stack_a->content;
 	while (ft_lstsize(stack_a) != 1)
 	{
@@ -54,23 +52,32 @@ void	sort100(t_list **stack_a, t_list **stack_b)
 	int index;
 	int j;
 	int k;
+	int h;
 
 	len = ft_lstsize(*stack_a);
 	k = ft_lstsize(*stack_a) / 4;
+	h = howmuch100(*stack_a, k);
 	while (ft_lstsize(*stack_a) != 0)
 	{
-		index = index100(*stack_a, k);
-		j = len - index + 1;
-		if (index < (len / 2))
-		while (index-- > 1)
-			ra(stack_a);
-		else
+		//printf("%d\n", k);
+		while (h-- != 0)
 		{
-			while(j-- > 0)
-				rra(stack_a);
+			index = index100(*stack_a, k);
+			j = len - index + 1;
+			if (index < (len / 2))
+			while (index-- > 1)
+				ra(stack_a);
+			else
+			{
+				while(j-- > 0)
+					rra(stack_a);
+			}
+			pb(stack_a, stack_b);
+			len = ft_lstsize(*stack_a);
 		}
-		pb(stack_a, stack_b);
-		len = ft_lstsize(*stack_a);
+		k = k + k;
+		h = howmuch100(*stack_a, k);
+		//printf("%d\n", h);
 	}
 }
 int main (int ac, char **av)
@@ -86,7 +93,7 @@ int main (int ac, char **av)
 		ft_creatlst(&stack_a, atoi(av[i]));
 		i++;
 	}
-	howmuch100(stack_a, 3);
+	sort100(&stack_a, &stack_b);
 	ft_print(stack_b);
 	return (0);
 }
